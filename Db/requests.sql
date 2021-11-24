0c10505455fe8a0fb3191aaa5262291e7a7cb84a
suggy1012 <77641000+suggy1012@users.noreply.github.com>
2021-11-24 15:34:08 +0300

    ok

---
+++ Db/requests.sql
@@ -0,0 +1,13 @@
+UPDATE Users
+SET Date_of_registration = substr(Date_of_registration, 7, 4) || substr(Date_of_registration, 4, 2) || substr(Date_of_registration, 1, 2);
+
+SELECT Login FROM Users
+WHERE Date_of_registration = (SELECT MAX(Date_of_registration) FROM Users);
+
+
+SELECT DISTINCT(substr(Date_of_birthday, 1, 4)) FROM USERS;
+
+SELECT Id , count(*) AS total_items  FROM Products;
+
+SELECT AVG(date('now') - Date_of_birthday) FROM Users
+WHERE date('now', '-2 months') >= Date_of_registration;
